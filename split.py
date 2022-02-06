eeeeb4b9ef388a882f92706c457f8c441f4dc03b
kai-lee1 <66237421+kai-lee1@users.noreply.github.com>
2022-02-05 22:28:43 -0800

    better

--- split.py
+++ split.py
@@ -1,9 +1,20 @@
 import cv2
-vidcap = cv2.VideoCapture('IMG_3164.mp4')
+vidcap = cv2.VideoCapture('pokpo1.mov')
 success,image = vidcap.read()
 count = 0
+frames = []
 while success:
-  cv2.imwrite("frame%d.jpg" % count, image)     # save frame as JPEG file
+  frames.append(image)
+  #cv2.imwrite("frame%d.jpg" % count, image)     # save frame as JPEG file
   success,image = vidcap.read()
   print('Read a new frame: ', success)
   count += 1
+
+dst = frames[0]
+
+for i in range(1, len(frames), 1):
+    print('frame%d.jpg' % i)
+    img = frames[i]
+    dst = cv2.addWeighted(dst, 0.95, img, 0.05, 0)
+cv2.imshow("result", dst)
+cv2.waitKey(0)
